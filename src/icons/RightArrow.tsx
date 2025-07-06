5a2a07e2f48564fdedec5285fb0772687ee19daa
Surya <surya040401@gmail.com>
2025-07-05 21:25:43 -0700

    fix: lint error

--- src/icons/RightArrow.tsx
+++ src/icons/RightArrow.tsx
@@ -7,6 +7,7 @@ const RightArrow = (props: React.SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 20 20"
     fill="currentColor"
+    {...props}
   >
     <path
       fillRule="evenodd"
