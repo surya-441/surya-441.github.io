086148df735d7175d403bcab404a806eacd99df5
Surya Siddharthan Sivakumar <surya040401@gmail.com>
2025-07-24 13:42:40 -0700

    swap work experience for projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,27 @@
 import ThemeToggle from "@/components/ThemeToggle";
-import WorkExperience from "@/components/WorkExperience";
+import Projects from "@/components/Projects";
 
 export default function Home() {
     return (
         <section className="bg-background text-foreground">
-            <div className="max-w-screen-xl px-4 py-8 mx-auto lg:px-6 sm:py-16 lg:py-24">
+            <div className="max-w-screen-xl px-4 py-8 mx-auto">
                 <div className="max-w-2xl mx-auto text-center">
                     <ThemeToggle />
                     <h2 className="text-3xl font-extrabold leading-tight tracking-tight text-foreground sm:text-4xl">
                         Surya Sivakumar
                     </h2>
                     <p className="mt-4 text-base font-normal text-foreground sm:text-xl">
-                        Curious and driven software engineer building elegant,
-                        practical solutions at the intersection of code and
-                        creativity.
+                        Full-stack developer and machine learning enthusiast
+                        with a 4.0 GPA Master’s in Computer Science from Arizona
+                        State University. I’ve architected secure, scalable
+                        systems across React, Node.js, and Spark ecosystems, and
+                        led impactful projects in GIS, computer vision, and NLP.
+                        Whether it’s building production-grade systems or
+                        pushing the edge in AI, I bring clarity, speed, and
+                        precision to every project I touch.
                     </p>
                 </div>
-                <WorkExperience />
+                <Projects />
             </div>
         </section>
     );
---
+++ src/components/Projects.tsx
@@ -0,0 +1,27 @@
+import { projects } from "../projects";
+
+const Projects = () => {
+    return (
+        <>
+            <div className="mt-8 text-3xl text-center font-extrabold leading-tight tracking-tight text-foreground sm:text-4xl">
+                Curated Projects
+            </div>
+            <div className="grid grid-cols-2 mt-12 text-center sm:mt-16 gap-x-20 gap-y-12 sm:grid-rows-2 lg:grid-rows-3">
+                {projects.map((project) => {
+                    return (
+                        <div className="space-y-4" key={project.title}>
+                            <h3 className="text-2xl font-bold leading-tight text-foreground">
+                                {project.title}
+                            </h3>
+                            <p className="text-lg font-normal text-foreground">
+                                {project.description}
+                            </p>
+                        </div>
+                    );
+                })}
+            </div>
+        </>
+    );
+};
+
+export default Projects;
